3f6d472a212fda59cdbaddabfe5c4d421c96cf10
viveksahu06 <viveksahu1006@gmail.com>
2025-01-07 16:08:26 +0530

    first

---
+++ Calc.java
@@ -0,0 +1,10 @@
+public class Calc {
+
+       public static void main(String[] args) {
+        System.out.println("Addition: " + add(10, 5));
+        System.out.println("Subtraction: " + subtract(10, 5));
+        System.out.println("Multiplication: " + multiply(10, 5));
+        System.out.println("Division: " + divide(10, 5));
+    }
+
+}
