3237502839486b771448fa63e6f08eef4d2d0552
viveksahu06 <viveksahu1006@gmail.com>
2025-01-07 15:57:52 +0530

    Add addition functionality

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Calculator!");
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
