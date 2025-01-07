94af86fa135c9f03c464c52a8a5e516fc6f4c479
viveksahu06 <viveksahu1006@gmail.com>
2025-01-07 16:09:49 +0530

    addition added

--- Calc.java
+++ Calc.java
@@ -1,10 +1,5 @@
 public class Calc {
-
-       public static void main(String[] args) {
-        System.out.println("Addition: " + add(10, 5));
-        System.out.println("Subtraction: " + subtract(10, 5));
-        System.out.println("Multiplication: " + multiply(10, 5));
-        System.out.println("Division: " + divide(10, 5));
+         public static int add(int a, int b) {
+        return a + b;
     }
-
 }
